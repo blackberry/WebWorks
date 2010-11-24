8d8bc5dfae90a75fcdd5f3015565fd0e68dda6ea
Peter Wu <yiwu@rim.com>
2010-11-24 15:58:48 -0500

    multi-sms sending patch

--- api/message/src/blackberry/message/sms/FunctionSendSMS.java
+++ api/message/src/blackberry/message/sms/FunctionSendSMS.java
@@ -1,19 +1,21 @@
 /*
-* Copyright 2010 Research In Motion Limited.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2010 Research In Motion Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package blackberry.message.sms;
+
+import java.util.Vector;
 
 import javax.microedition.io.Connection;
 import javax.microedition.io.Connector;
@@ -32,51 +34,82 @@ import net.rim.device.api.script.ScriptableFunction;
  */
 public class FunctionSendSMS extends ScriptableFunction {
 
-    public static final String NAME = "send";
+	public static final String NAME = "send";
 
-    private Connection _conn;
+	private static Vector _senders;
+	private Connection _conn;
 
-    public Object invoke(Object thiz, Object[] args) {
-        ArgumentValidationUtil.validateParameterNumber(new int[] { 2 }, args);
-        String msgContent = args[0].toString();
-        String msgAddress = args[1].toString();
+	public FunctionSendSMS() {
+		if (_senders == null) {
+			_senders = new Vector();
+			new Thread() {
+				public void run() {
+					while (true) {
+						synchronized (_senders) {
+							if (_senders.isEmpty()) {
+								try {
+									_senders.wait();
+								} catch (InterruptedException e) {
+									return;
+								}
+							}
 
-        SMSMessage msg = new SMSMessage(msgContent, msgAddress);
-        Sender sender = new Sender(msg);
-        sender.start();
+							if (!_senders.isEmpty()) {
+								((Sender) _senders.elementAt(0)).start();
+								_senders.removeElementAt(0);
+							}
+						}
+					}
+				}
+			}.start();
+		}
+	}
 
-        return UNDEFINED;
-    }
+	public Object invoke(Object thiz, Object[] args) {
+		ArgumentValidationUtil.validateParameterNumber(new int[] { 2 }, args);
+		String msgContent = args[0].toString();
+		String msgAddress = args[1].toString();
 
-    private class Sender extends Thread {
-        SMSMessage message;
+		SMSMessage msg = new SMSMessage(msgContent, msgAddress);
 
-        public Sender(SMSMessage msg) {
-            message = msg;
-        }
+		synchronized (_senders) {
+			_senders.addElement(new Sender(msg));
+			_senders.notifyAll();
+		}
+		return UNDEFINED;
+	}
 
-        public void run() {
-            try {
-                _conn = Connector.open(SMSMessage.PROTOCOL + message.getAddress());
+	private class Sender extends Thread {
+		SMSMessage message;
 
-                if (MessageUtil.isCDMA()) {
-                    DatagramConnectionBase dcb = (DatagramConnectionBase) _conn;
-                    dcb.send(message.toDatagram(dcb));
-                }
-                else {
-                    MessageConnection mc = (MessageConnection) _conn;
-                    mc.send(message.toMessage(mc));
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage());
-            } finally {
-                try {
-                    if (_conn != null) {
-                        _conn.close();
-                    }
-                } catch (Exception e) {
-                }
-            }
-        }
-    }
+		public Sender(SMSMessage msg) {
+			message = msg;
+		}
+
+		public void run() {
+			synchronized (_senders) {
+				try {
+					_conn = Connector.open(SMSMessage.PROTOCOL
+							+ message.getAddress());
+
+					if (MessageUtil.isCDMA()) {
+						DatagramConnectionBase dcb = (DatagramConnectionBase) _conn;
+						dcb.send(message.toDatagram(dcb));
+					} else {
+						MessageConnection mc = (MessageConnection) _conn;
+						mc.send(message.toMessage(mc));
+					}
+				} catch (Exception e) {
+					throw new RuntimeException(e.getMessage());
+				} finally {
+					try {
+						if (_conn != null) {
+							_conn.close();
+						}
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+	}
 }
